f57eab350af328bf75f7e82933f1a9957a394dc7
zaru6 <markoruza1@gmail.com>
2024-05-06 19:35:06 +0200

    submit product button fucntional

--- src/app/product-creator/product-creator.component.ts
+++ src/app/product-creator/product-creator.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-creator',
-  templateUrl: `product-creator.component.html`
+  templateUrl: './product-creator.component.html'
 })
 export class ProductCreatorComponent {
   product = {
     name: '',
     price: 0,
-    available: 0
+    available: false // Initialize available as a boolean
   };
+  productMessage: string = '';
+
+  constructor(private http: HttpClient) { }
 
   addProduct(product: any) {
-    // Insert the product object into the database here
+    this.http.post('http://localhost:8080/api/products', product)
+      .subscribe(response => {
+        this.productMessage = 'Object insertion successful';
+        console.log(response);
+        // You can also navigate to a different route or display a success message here
+      }, error => {
+        this.productMessage = 'Object insertion failed';
+        console.error(error);
+        // You can also display an error message here
+      });
   }
 }
