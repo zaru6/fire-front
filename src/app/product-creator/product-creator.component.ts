cecb2ea0f0e5e57caae713e21f683d6fe16baca1
zaru6 <markoruza1@gmail.com>
2024-05-07 22:45:55 +0200

    delete button added

--- src/app/product-creator/product-creator.component.ts
+++ src/app/product-creator/product-creator.component.ts
@@ -20,11 +20,10 @@ export class ProductCreatorComponent {
       .subscribe(response => {
         this.productMessage = 'Object insertion successful';
         console.log(response);
-        // You can also navigate to a different route or display a success message here
       }, error => {
         this.productMessage = 'Object insertion failed';
         console.error(error);
-        // You can also display an error message here
       });
   }
+  
 }
--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { Product } from '../product.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-products-table',
@@ -9,12 +10,26 @@ import { Product } from '../product.model';
 })
 export class ProductsTableComponent implements OnInit {
   products: Product[] = [];
+  productMessage: string = '';
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.apiService.getProducts().subscribe(products => {
       this.products = products;
+    });
+  }
+
+  deleteProduct(productId: number) {
+    this.http.delete(`http://localhost:8080/api/products/${productId}`)
+    .subscribe(response => {
+        this.productMessage = 'Product deletion successful';
+        console.log(response);
+        // Remove the product from the products array
+        this.products = this.products.filter(product => product.id!== productId);
+      }, error => {
+        this.productMessage = 'Product deletion failed';
+        console.error(error);
     });
   }
 }
