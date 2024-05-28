43777f7142865de1e2edaf69bfd4b4ebea096f1b
zaru6 <markoruza1@gmail.com>
2024-05-28 18:09:11 +0200

    DTO product table complete

--- src/app/productdtos/productdtos.component.ts
+++ src/app/productdtos/productdtos.component.ts
@@ -5,6 +5,10 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UploadComponent } from '../upload/upload.component';
+import { ProductFormComponent } from '../product-form/product-form.component';
+import { ProductFormUpdateComponent } from '../product-form-update/product-form-update.component';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-productdtos',
@@ -30,6 +34,64 @@ export class ProductdtosComponent {
     this.productService.getProductDtos().subscribe(products => {
       this.products = products;
     });
+  }
+
+  deleteProduct(productId: number) {
+    this.productService.deleteProduct(productId).subscribe(
+      () => {
+        console.log("Product deleted successfully");
+        this.getProductDtos();
+      },
+      (error) => {
+        console.error("Error deleting product:", error);
+      }
+    );
+  }
+
+  onUpdateProduct(product: ProductDTO) {
+    const modalRef = this.modalService.open(ProductFormUpdateComponent);
+    modalRef.componentInstance.product = product; 
+    modalRef.componentInstance.closeButtonClick.subscribe(() => {
+      modalRef.close();
+    });
+    modalRef.result.then(
+      result => {
+        console.log('Closed with:', result);
+      },
+      reason => {
+        console.log('Dismissed', reason);
+      }
+    );
+  }
+
+  openProductForm(): void {
+    const modalRef = this.modalService.open(ProductFormComponent);
+    modalRef.componentInstance.closeButtonClick.subscribe(() => {
+      modalRef.close();
+    });
+    modalRef.result.then(
+      result => {
+        console.log('Closed with:', result);
+      },
+      reason => {
+        console.log('Dismissed', reason);
+      }
+    );
+  }
+
+  openUploadForm(): void {
+    const modalRef = this.modalService.open(UploadComponent);
+    modalRef.componentInstance.closeButtonClick.subscribe(() => {
+      modalRef.close();
+    });
+    modalRef.result.then(
+      result => {
+        console.log('Closed with:', result);
+      },
+      reason => {
+        console.log('Dismissed', reason);
+      }
+    );
   }
 
 }
